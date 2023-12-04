ac6207a15a879eb664b86efd28b71112f7d55662
AniSindhu <aniruddhsindhu@gmail.com>
2023-12-04 02:02:27 -0800

    all updates

--- Exercise-04-Template-master/src/Overworld.js
+++ Exercise-04-Template-master/src/Overworld.js
@@ -9,17 +9,52 @@ class Overworld extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         })
+
+        this.load.image('tilesetImage' , 'tileset.png')
+        this.load.tilemapTiledJSON('tilemapJSON' , 'overworld.json')
     }
 
     create() {
         // velocity constant
         this.VEL = 100
 
+        //ORDER OF Z AXIS IS ORDER OF WHICH IS IMPLEMENTED
+        // add tilemap
+        const map = this.add.tilemap('tilemapJSON')
+        // name of the tileset created in TILED , name of key
+        const tileset = map.addTilesetImage('tileset', 'tilesetImage')
+        // name of the layer given in TILED
+        const bgLayer = map.createLayer('Background', tileset, 0, 0)
+        const terrainLayer = map.createLayer('Terrain', tileset, 0, 0)
+        const treeLayer = map.createLayer('Trees', tileset, 0, 0)
+
         // add slime
-        this.slime = this.physics.add.sprite(32, 32, 'slime', 0)
+        const slimeSpawn = map.findObject('Spawns', obj => obj.name === 'slimeSpawn')
+        this.slime = this.physics.add.sprite(slimeSpawn.x, slimeSpawn.y, 'slime', 0)
         this.slime.body.setCollideWorldBounds(true)
 
         // slime animation
+        this.anims.create({
+            key: 'jiggle',
+            frameRate: 4,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('slime', {
+                start: 0,
+                end: 1
+            })
+        })
+        this.slime.play('jiggle')
+
+        // camera
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+        this.cameras.main.startFollow(this.slime, true, 0.25, 0.25)
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
+
+        // collisions
+        terrainLayer.setCollisionByProperty({
+            collides: true
+        })
+        this.physics.add.collider(this.slime, terrainLayer)
 
         // input
         this.cursors = this.input.keyboard.createCursorKeys()
